9894797a2dc67b6cd0320600fe6df74847a4ce30
anhadhh21 <anhadhmeshri21@gmail.com>
2024-07-31 00:00:53 +0530

    air pollution component

--- app/Components/Temperature/Temperature.tsx
+++ app/Components/Temperature/Temperature.tsx
@@ -1,6 +1,6 @@
 "use client";
-import React, {useState} from 'react'
-import {useGlobalContext } from "@/app/context/globalContext";
+import React, {useState, useEffect} from 'react'
+import {useGlobalContext} from "@/app/context/globalContext";
 import {
     clearSky,
     cloudy,
@@ -10,9 +10,10 @@ import {
     snow,
   } from "@/app/utils/Icons";
 import { kelvinToCelsius } from "@/app/utils/misc";
+import moment from 'moment';
 
 function Temperature() {
-    const { forecast } = useGlobalContext();
+    const {forecast} = useGlobalContext();
     const { main, timezone, name, weather } = forecast;
 
     if (!forecast || !weather) {
@@ -44,20 +45,25 @@ function Temperature() {
         return clearSky;
     }
   };
+  // Live time update
+  useEffect(() => {
+    // upadte time every second
+    const interval = setInterval(() => {
+      const localMoment = moment().utcOffset(timezone / 60);
+      // custom format: 24 hour format
+      const formatedTime = localMoment.format("HH:mm:ss");
+      // day of the week
+      const day = localMoment.format("dddd");
 
+      setLocalTime(formatedTime);
+      setCurrentDay(day);
+    }, 1000);
 
+    // clear interval
+    return () => clearInterval(interval);
+  }, [timezone]);
 
-
-
-
-
-
-
-
-
-
-
-    return <div
+    return ( <div
     className="pt-6 pb-5 px-4 border rounded-lg flex flex-col 
       justify-between dark:bg-dark-grey shadow-sm dark:shadow-none"
   >
@@ -65,7 +71,24 @@ function Temperature() {
         <span className="font-medium">{currentDay}</span>
         <span className="font-medium">{localTime}</span>
       </p>
+      <p className="pt-2 font-bold flex gap-1">
+        <span>{name}</span>
+        <span>{navigation}</span>
+      </p>
+      <p className="py-10 text-9xl font-bold self-center">{temp}°</p>
+
+      <div>
+        <div>
+          <span>{getIcon()}</span>
+          <p className="pt-2 capitalize text-lg font-medium">{description}</p>
+        </div>
+        <p className="flex items-center gap-2">
+          <span>Low: {minTemp}°</span>
+          <span>High: {maxTemp}°</span>
+        </p>
+      </div>
   </div>
+    );
 }
 
 export default Temperature;
--- app/context/globalContext.js
+++ app/context/globalContext.js
@@ -7,6 +7,7 @@ const GlobalContextUpdate = createContext();
 
 export const GlobalContextProvider = ({children}) => {
     const [forecast, setForecast] = useState({});
+    const [airQuality, setAirQuality] = useState({});
 
     const fetchForecast = async () => {
         try {
@@ -16,15 +17,30 @@ export const GlobalContextProvider = ({children}) => {
             console.log("Error fetching forecast data: ", error.message);
         }
         };
+        // Air Quality
+  const fetchAirQuality = async (lat, lon) => {
+    try {
+      const res = await axios.get(`api/pollution?lat=${lat}&lon=${lon}`);
+      setAirQuality(res.data);
+    } catch (error) {
+      console.log("Error fetching air quality data: ", error.message);
+    }
+  };
+
+
+
 
         useEffect(() => {
             fetchForecast();
+            fetchAirQuality();
         }, []);
 
         return (
             <GlobalContext.Provider value = {{
-                
-            }}>
+              forecast,
+              airQuality,
+            }}
+            >
                 <GlobalContextUpdate.Provider>{children}</GlobalContextUpdate.Provider>
             </GlobalContext.Provider>
         );
