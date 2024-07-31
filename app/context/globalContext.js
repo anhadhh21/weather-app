82526b61e334b37121a24093b06aeb5597a2e5f9
anhadhh21 <anhadhmeshri21@gmail.com>
2024-08-01 00:28:08 +0530

    various components

--- app/context/globalContext.js
+++ app/context/globalContext.js
@@ -8,6 +8,7 @@ const GlobalContextUpdate = createContext();
 export const GlobalContextProvider = ({children}) => {
     const [forecast, setForecast] = useState({});
     const [airQuality, setAirQuality] = useState({});
+    const [uvIndex, setUvIndex] = useState({});
 
     const fetchForecast = async () => {
         try {
@@ -26,6 +27,27 @@ export const GlobalContextProvider = ({children}) => {
       console.log("Error fetching air quality data: ", error.message);
     }
   };
+ // five day forecast
+ const fetchFiveDayForecast = async (lat, lon) => {
+    try {
+      const res = await axios.get(`api/fivedays?lat=${lat}&lon=${lon}`);
+
+      setFiveDayForecast(res.data);
+    } catch (error) {
+      console.log("Error fetching five day forecast data: ", error.message);
+    }
+  };
+
+  //fetch uv data
+  const fetchUvIndex = async (lat, lon) => {
+    try {
+      const res = await axios.get(`/api/uv?lat=${lat}&lon=${lon}`);
+
+      setUvIndex(res.data);
+    } catch (error) {
+      console.error("Error fetching the forecast:", error);
+    }
+  };
 
 
 
@@ -33,6 +55,8 @@ export const GlobalContextProvider = ({children}) => {
         useEffect(() => {
             fetchForecast();
             fetchAirQuality();
+             fetchFiveDayForecast();
+             fetchUvIndex();            
         }, []);
 
         return (
